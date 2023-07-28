e58c869842b1e298f22bb58f8f24423a61bd56ba
Romina Guerreiro <guerreiromina@gmail.com>
2023-07-28 16:10:15 -0300

    modificacion componente Store, map

---
+++ proyecto-final/src/assets/articulos.js
@@ -0,0 +1,85 @@
+import cablered from './cablered.png';
+import adaptador from './adaptador.png';
+import bateria from './bateria.png';
+import fuente from './fuente.png';
+import multimetro from './multimetro.png';
+import pinzapelacables from './pinzapelacables.png';
+import protector from './protector.png';
+import soldador from './soldador.png';
+import zapatilla from './zapatilla.png';
+import tiraled from './tiraled.png';
+
+const articulos = [
+    {
+        id: 1,
+        image: cablered,
+        title: "Cable de red",
+        description: "Cable de red de 5m",
+        price: "$ 1500",
+    },
+    {
+        id: 2,
+        image: adaptador,
+        title: "Adaptador",
+        description: "Adaptador de enchufe",
+        price: "$ 489",
+    },
+    {
+        id: 3,
+        image: bateria,
+        title: "Batería",
+        description: "Batería modelo X5478",
+        price: "$ 6750",
+    },
+    {
+        id: 4,
+        image: fuente,
+        title: "Fuente",
+        description: "Fuente modelo WX1575",
+        price: "$ 1890",
+    },
+    {
+        id: 5,
+        image: multimetro,
+        title: "Multímetro",
+        description: "Multímetro digital",
+        price: "$ 5680",
+    },
+    {
+        id: 6,
+        image: pinzapelacables,
+        title: "Pinza pela cables",
+        description: "Pinza pela cables",
+        price: "$ 4500",
+    },
+    {
+        id: 7,
+        image: protector,
+        title: "Protector de tensión",
+        description: "Para electrodomésticos",
+        price: "$ 3900",
+    },
+    {
+        id: 8,
+        image: soldador,
+        title: "Soldador",
+        description: "Soldador standard",
+        price: "$ 2350",
+    },
+    {
+        id: 9,
+        image: zapatilla,
+        title: "Zapatilla",
+        description: "Zapatilla de 5 enchufes",
+        price: "$ 5590",
+    },
+    {
+        id: 10,
+        image: tiraled,
+        title: "Tira LED",
+        description: "Tira LED de 1m",
+        price: "$ 2999",
+    },
+]
+
+export default articulos;
